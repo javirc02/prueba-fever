23ad754a32e1012d238928a0c1a9978cf828578b
javirc02 <javiroca89@gmail.com>
2025-06-04 18:04:15 +0200

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/fever/ecomerce/EcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fever.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
